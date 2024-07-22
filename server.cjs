39de2056260f8b33081d54d695345bd615a7d9f6
Jérémy Cloarec <jeremycloarec@msn.com>
2024-07-22 16:35:56 +0200

    Create function create file

--- server.cjs
+++ server.cjs
@@ -5,68 +5,60 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 4000;
+
 app.use(bodyParser.json());
 app.use(cors());
 
 const fileContent = require('./backend_fs/fileContent.cjs');
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('Hello on the form buider project')
 })
+
+// Fonction pour mettre à jour un fichier
+async function updateFile(filePath, referenceIndex, replaceContent) {
+    // Lire le fichier
+    let data = await fs.readFile(filePath, 'utf8');
+    // Trouver la position d'insertion
+    const insertPosition = data.lastIndexOf(referenceIndex);
+    // Si la position est trouvée, insérer le nouveau contenu
+    if (insertPosition !== -1) {
+        const before = data.slice(0, insertPosition);
+        const after = data.slice(insertPosition);
+
+        const updatedData = before + replaceContent + after;
+        await fs.writeFile(filePath, updatedData, 'utf-8')
+    }
+}
 
 app.post('/write-file', async (req, res) => {
     const { formId, formName } = req.body;
     let fileName = `${formId}-${formName.replace(/\s+/g, '-').toLowerCase()}`;
 
-    // Définir chemin et contenu du fichier
-    const filePath = path.join(__dirname, 'src/components/admin/forms_admin', `${fileName}.vue`);
+    // Définir les chemins des fichiers
+    const newFormFilePath = path.join(__dirname, 'src/components/admin/forms_admin', `${fileName}.vue`);
     const routerFilePath = path.join(__dirname, 'src/router.js');
     const adminHomFilePath = path.join(__dirname, 'src/components/admin/HomeAdmin.vue');
 
-    // Route à ajouter
-    const newRoute = `\n    { path: "/admin/${fileName}", component: () => import("./components/admin/forms_admin/${fileName}.vue") },`;
+    // Contenu à ajouer
+    const newRoute = `\n    { path: "/admin/${fileName}", component: () => import("./components/admin/forms_admin/${fileName}.vue") },\n`;
+    const newButton = `\n            <button @click="router.push('/admin/${fileName}')">Go to ${formName}</button>\n`;
 
+    // Générer le contenu du formulaire
     const generateFileContent = fileContent(formId);
 
     try {
         // Créer le fichier du nouveau formulaire
-        await fs.writeFile(filePath, generateFileContent);
+        await fs.writeFile(newFormFilePath, generateFileContent);
         console.log(`Le fichier ${fileName} a été créé avec succès`);
 
-        // Lire le fichier router.js
-        let dataRouter = await fs.readFile(routerFilePath, 'utf8');
+        // Mettre à jour le fichier router.js
+        await updateFile(routerFilePath, '// Les nouvelles routes seront ajoutées au-dessus de cette ligne', newRoute);
 
-        // Trouver la position d'insertion avant la création du routeur
-        const insertPositionRoute = dataRouter.lastIndexOf('const router = createRouter({');
+        // Mettre à jour le fichier HomeAdmin.vue
+        await updateFile(adminHomFilePath, ' <!-- Les boutons vers les nouveau formulaire sont ajoutés au-dessus de cette lignes -->', newButton);
 
-        // Si la position est trouvée, insérer la nouvelle route
-        if (insertPositionRoute !== -1) {
-            const before = dataRouter.slice(0, insertPositionRoute);
-            const after = dataRouter.slice(insertPositionRoute);
-
-            // Mise à jour du tableau de routes
-            const updatedRoutes = before.replace(']', `${newRoute}\n]`);
-            const updatedData = updatedRoutes + after;
-
-            await fs.writeFile(routerFilePath, updatedData, 'utf-8')
-        }
-
-        // Lire le fichier HomeAdmin.vue
-        let dataHomeAdmin = await fs.readFile(adminHomFilePath, 'utf8');
-        const insertPositionHomeAdmin = dataHomeAdmin.lastIndexOf(' <!-- Add button to new form befor this line -->');
-        console.log(insertPositionHomeAdmin);
-        if (insertPositionHomeAdmin !== -1) {
-            const beforeAdmin = dataHomeAdmin.slice(0, insertPositionHomeAdmin);
-            const afterAdmin = dataHomeAdmin.slice(insertPositionHomeAdmin);
-
-            const newButton = `
-                <button @click="router.push('/admin/${fileName}')">Go to ${formName}</button>
-            `;
-            const updatedHomeAdmin = beforeAdmin + newButton + afterAdmin;
-            await fs.writeFile(adminHomFilePath, updatedHomeAdmin, 'utf-8');
-        }
-
-
+        res.status(200).send(`Formulaire ${formName} créé et routes mises à jour.`);
     } catch (err) {
         console.error('Error writing file:', err);
         return res.status(500).send('Error writing file');
--- src/components/admin/HomeAdmin.vue
+++ src/components/admin/HomeAdmin.vue
@@ -16,36 +16,30 @@ const goToFormBuilder = () => {
         </button>
         <div class="container-news-forms">
             <h2>News forms</h2>
-            <button @click="router.push('/admin/122-aa')">Go to aa</button>
-            <button @click="router.push('/admin/123-aa')">Go to aa</button>
-           
-                <button @click="router.push('/admin/124-aaa')">Go to aaa</button>
-            
-                <button @click="router.push('/admin/127-a')">Go to a</button>
-            
-                <button @click="router.push('/admin/128-yoyo')">Go to yoyo</button>
-             <!-- Add button to new form befor this line -->
+
+            <!-- Les boutons vers les nouveau formulaire sont ajoutés au-dessus de cette lignes -->
         </div>
     </div>
 </template>
 
 <style>
 .container-admin {
-    padding : 16px
+    padding: 16px
 }
+
 .form-builder {
     background-color: black;
     color: white;
 }
 
-.container-news-forms{
+.container-news-forms {
     display: flex;
     flex-direction: column;
     gap: 16px;
     background-color: rgb(233, 233, 233);
     padding: 16px;
     max-width: 800px;
-    margin : auto;
+    margin: auto;
     margin-top: 34px;
     border-radius: 12px;
 }
